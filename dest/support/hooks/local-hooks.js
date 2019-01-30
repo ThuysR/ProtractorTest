d00a79cd768dd4ffa21101582b3bfc2ee760c4fc
ThuysR <roland.thuys@tvh.com>
2019-01-30 08:51:35 +0100

    remove file

--- dest/support/hooks/local-hooks.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const { BeforeAll, After, Before, Status, setDefaultTimeout } = require("cucumber");
-const baseFile_1 = require("../../config/baseFile");
-const baseHooks_1 = require("./baseHooks");
-baseHooks_1.executeHooks(baseFile_1.localBaseFile);
